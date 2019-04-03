83db2337de15ec4971abb22f0b2876cf49366402
Fede <fjgimenez@miuandes.cl>
2019-04-03 11:08:41 -0300

    ticket type validation done

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,4 +1,5 @@
 class TicketType < ApplicationRecord
   belongs_to :event
   belongs_to :ticket_zone
+	validates :price, numericality: { greater_than_or_equal_to: 0, :message => "price must be greater of equal to 0"}
 end
