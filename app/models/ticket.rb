4af5a13893f6d3f0898a9822feb10d7ae05bac54
Emilio <eivelez@miuandes.cl>
2019-04-04 20:34:31 -0300

    NO past ticket creation, constraint added)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,10 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+	before_validation :valid_date
+	def valid_date
+		if ticket_type.event.start_date<Date.today
+			errors.add(:ticket, "can't be in the past")
+		end
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,11 @@ for ev in event_venues do
 end 
 
 events = [Event.new(name:'Festival de la Cancion', description: 'Puro reggaeton',\
-          start_date: '2019-03-01', event_venue: event_venues[0]),
+          start_date: '2019-06-01', event_venue: event_venues[0]),
           Event.new(name:'Twisted Sister', description: 'Puro rock',\
-          start_date: '2019-04-01', event_venue: event_venues[1]),
+          start_date: '2019-12-12', event_venue: event_venues[1]),
           Event.new(name:'Bad Bunny', description: 'Puro Trap',\
-          start_date: '2019-05-01', event_venue: event_venues[2])]
+          start_date: '2019-04-04', event_venue: event_venues[2])]
 
 for e in events do
   e.save!
